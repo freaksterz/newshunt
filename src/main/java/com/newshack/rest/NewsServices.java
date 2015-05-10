9f68b9c64afd04ae2abc34c3bffbf18934fde975
freaksterz <vickyknowsu@gmail.com>
2015-05-10 08:27:32 +0530

    Working code with restfull

--- src/main/java/com/newshack/rest/NewsServices.java
+++ src/main/java/com/newshack/rest/NewsServices.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,16 +16,34 @@ import java.util.Map;
  * Created by freakster on 9/5/15.
  */
 
-@Path("/content")
+@Path("/contents")
 public class NewsServices {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public String getRelatedContent(String req )
     {
+        String[] splited = null;
+        List<Map> mapList = new ArrayList<>();
         NewsContent newsContent = new NewsContent();
 
-        List<Map> mapList= newsContent.getContent(req);
+        if(req.contains(" ")) {
+            splited = req.split("\\s+");
+
+
+
+            for (String spli : splited) {
+                if (newsContent.getContent(spli) != null) {
+                    mapList = (newsContent.getContent(req));
+                    break;
+                }
+            }
+        }
+        else
+        {
+            mapList = (newsContent.getContent(req));
+        }
+
 
         //convert it back to json and send
         JSONArray array = new JSONArray(mapList);
